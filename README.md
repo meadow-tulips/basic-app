847cbb9971c9e01aee87a8449d06555cd2c1e4aa
Avan Sharma <avan@zopsmart.com>
2021-05-27 17:04:10 +0530

    Initial-commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Do `yarn && yarn start`.
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import QuoteMachine from './components/QuoteMachine'
+import logo from './logo.svg';
+import './App.css';
+import './style.css'
+
+function App() {
+  return (
+    <div className="App">
+      <QuoteMachine />
+    </div>
+  );
+}
+
+export default App;
